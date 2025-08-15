cfea5ebdfc14b1bc5483434b02944dbb4477d8ca
Furkan Kambay <furkankambay@protonmail.com>
2025-08-15 18:22:45 +0300

    unit hurt flash shader

--- Assets/Scripts/UnitAttackVisual.cs
+++ Assets/Scripts/UnitAttackVisual.cs
@@ -1,16 +1,55 @@
+using System.Collections;
 using UnityEngine;
 
 namespace FurkanKambay
 {
     public class UnitAttackVisual : MonoBehaviour
     {
-        [SerializeField] private Attacker attacker;
+        [Header("References")]
+        [SerializeField] private SpriteRenderer spriteRenderer;
         [SerializeField] private Animator animator;
+        [SerializeField] private Attacker attacker;
+        [SerializeField] private Vitality vitality;
+
+        [Header("Config")]
+        [SerializeField] private float hurtDuration;
+
+        private MaterialPropertyBlock propertyBlock;
 
         private static readonly int AnimAttack = Animator.StringToHash("attack");
+        private static readonly int ShaderHurt = Shader.PropertyToID("_Hurt");
 
-        private void OnEnable()  => attacker.OnAttackStarted += Attacker_AttackStarted;
-        private void OnDisable() => attacker.OnAttackStarted -= Attacker_AttackStarted;
+        private void Awake()
+        {
+            propertyBlock = new MaterialPropertyBlock();
+            propertyBlock.SetInt(ShaderHurt, 0);
+            spriteRenderer.SetPropertyBlock(propertyBlock);
+        }
+
+        private void OnEnable()
+        {
+            attacker.OnAttackStarted += Attacker_AttackStarted;
+            vitality.OnDamageTaken   += Vitality_DamageTaken;
+        }
+
+        private void OnDisable()
+        {
+            attacker.OnAttackStarted -= Attacker_AttackStarted;
+            vitality.OnDamageTaken   -= Vitality_DamageTaken;
+        }
+
+        private void Vitality_DamageTaken() =>
+            StartCoroutine(GetHurt());
+
+        private IEnumerator GetHurt()
+        {
+            propertyBlock.SetInt(ShaderHurt, 1);
+            spriteRenderer.SetPropertyBlock(propertyBlock);
+            yield return new WaitForSeconds(hurtDuration);
+
+            propertyBlock.SetInt(ShaderHurt, 0);
+            spriteRenderer.SetPropertyBlock(propertyBlock);
+        }
 
         private void Attacker_AttackStarted()
         {
