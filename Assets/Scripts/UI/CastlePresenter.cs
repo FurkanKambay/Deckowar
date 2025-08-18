c520750a1dc78d9fef9949a128a15e0d79b0d913
Furkan Kambay <furkankambay@protonmail.com>
2025-08-18 07:44:50 +0300

    better UI

--- Assets/Scripts/UI/CastlePresenter.cs
+++ Assets/Scripts/UI/CastlePresenter.cs
@@ -23,14 +23,14 @@ namespace FurkanKambay.UI
 
             [Header("UI")]
             [SerializeField] private Image bar;
-            [SerializeField] private Image    queueRadial;
+            [SerializeField] private Image spawnRadial;
             [SerializeField] private TMP_Text queueLabel;
 
             public void UpdateUI()
             {
                 bar.rectTransform.localScale = new Vector3(castle.Vitality.HealthNormalized, 1, 1);
 
-                queueRadial.fillAmount = castle.ProgressUntilNextSpawn;
+                spawnRadial.fillAmount = castle.ProgressUntilNextSpawn;
                 queueLabel.alpha       = castle.SpawnQueueCount == 0 ? 0 : 1;
                 queueLabel.text        = castle.SpawnQueueCount.ToString();
             }
