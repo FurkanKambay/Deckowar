07bf1e7efeab6768148e6b7daba2cb2dadae47bc
Furkan Kambay <furkankambay@protonmail.com>
2025-08-18 05:59:38 +0300

    queue UI stuff

--- Assets/Scripts/UI/CastleHealthUI.cs
+++ Assets/Scripts/UI/CastleHealthUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,17 +7,30 @@ namespace FurkanKambay.UI
     public class CastleHealthUI : MonoBehaviour
     {
         [Header("References")]
+        [SerializeField] private Castle playerCastle;
+        [SerializeField] private Castle   enemyCastle;
         [SerializeField] private Vitality playerVitality;
         [SerializeField] private Vitality enemyVitality;
 
         [Header("UI")]
         [SerializeField] private Image playerBar;
-        [SerializeField] private Image enemyBar;
+        [SerializeField] private Image    enemyBar;
+        [SerializeField] private TMP_Text playerQueueLabel;
+        [SerializeField] private TMP_Text enemyQueueLabel;
 
         private void Update()
         {
             playerBar.rectTransform.localScale = new Vector3(playerVitality.HealthNormalized, 1, 1);
             enemyBar.rectTransform.localScale  = new Vector3(enemyVitality.HealthNormalized,  1, 1);
+
+            UpdateQueueLabel(playerQueueLabel, playerCastle.SpawnQueueCount);
+            UpdateQueueLabel(enemyQueueLabel,  enemyCastle.SpawnQueueCount);
+        }
+
+        private static void UpdateQueueLabel(TMP_Text label, int queueCount)
+        {
+            label.alpha = queueCount == 0 ? 0 : 1;
+            label.text  = queueCount.ToString();
         }
     }
 }
