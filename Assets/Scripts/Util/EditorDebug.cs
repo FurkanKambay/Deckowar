0f09d292e24910967dea5cce0b19bb48c17e9bc3
Furkan Kambay <furkankambay@protonmail.com>
2025-10-12 16:04:01 +0300

    EditorDebug

---
+++ Assets/Scripts/Util/EditorDebug.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+
+namespace FurkanKambay.Util
+{
+    public static class EditorDebug
+    {
+        [HideInCallstack, Conditional("UNITY_EDITOR")]
+        public static void Log(string message) => Debug.Log(message);
+
+        [HideInCallstack, Conditional("UNITY_EDITOR")]
+        public static void Log(string message, Object context) => Debug.Log(message, context);
+
+        [HideInCallstack, Conditional("UNITY_EDITOR")]
+        public static void LogWarning(string message) => Debug.LogWarning(message);
+
+        [HideInCallstack, Conditional("UNITY_EDITOR")]
+        public static void LogWarning(string message, Object context) => Debug.LogWarning(message, context);
+
+        [HideInCallstack, Conditional("UNITY_EDITOR")]
+        public static void LogError(string message) => Debug.LogError(message);
+
+        [HideInCallstack, Conditional("UNITY_EDITOR")]
+        public static void LogError(string message, Object c) => Debug.LogError(message, c);
+
+        [HideInCallstack, Conditional("UNITY_EDITOR")]
+        public static void LogException(Exception exception) => Debug.LogException(exception);
+
+        [HideInCallstack, Conditional("UNITY_EDITOR")]
+        public static void LogException(Exception exception, Object c) => Debug.LogException(exception, c);
+    }
+}
