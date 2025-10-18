eee9553324cd7ee510fb05373fc39de52aab9f04
Furkan Kambay <furkankambay@protonmail.com>
2025-10-18 16:25:12 +0300

    Heading ext: GetOpposite

--- Assets/Scripts/Core/Heading.cs
+++ Assets/Scripts/Core/Heading.cs
@@ -6,4 +6,14 @@ namespace Deckowar.Core
         West,
         East
     }
+
+    public static class HeadingExtensions
+    {
+        public static Heading GetOpposite(this Heading heading) => heading switch
+        {
+            Heading.West => Heading.East,
+            Heading.East => Heading.West,
+            _ => Heading.None
+        };
+    }
 }
