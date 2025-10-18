c98429f35405af2790655ca0973321d0ecd1335a
Furkan Kambay <furkankambay@protonmail.com>
2025-10-18 22:07:32 +0300

    BattleManager

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Deckowar
+{
+    public sealed class BattleManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Battlefield battlefield;
+    }
+}
