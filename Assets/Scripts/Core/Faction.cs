3ed6d70596c709f9ff4b5a0d2419093a60a033d7
Furkan Kambay <furkankambay@protonmail.com>
2025-10-18 23:39:39 +0300

    None faction + FactionExtensions GetHeading

--- Assets/Scripts/Core/Faction.cs
+++ Assets/Scripts/Core/Faction.cs
@@ -2,7 +2,18 @@ namespace Deckowar.Core
 {
     public enum Faction
     {
+        None,
         Player,
         Enemy
+    }
+
+    public static class FactionExtensions
+    {
+        public static Heading GetHeading(this Faction faction) => faction switch
+        {
+            Faction.Player => Heading.East,
+            Faction.Enemy => Heading.West,
+            _ => Heading.None
+        };
     }
 }
