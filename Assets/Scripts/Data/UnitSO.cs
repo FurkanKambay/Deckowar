f5c77148f5b978191e567984411397c7dd94c37a
Furkan Kambay <furkankambay@protonmail.com>
2025-10-19 12:21:19 +0300

    UnitSO.MaxHealth

--- Assets/Scripts/Data/UnitSO.cs
+++ Assets/Scripts/Data/UnitSO.cs
@@ -7,6 +7,7 @@ namespace Deckowar.Data
     {
         [Header("Spawn")]
         [SerializeField, Min(0)] private int spawnDelay;
+        [SerializeField, Min(0)] private int maxHealth = 1;
 
         [Header("Attack")]
         [SerializeField, Min(0)] private int damage = 1;
@@ -16,6 +17,7 @@ namespace Deckowar.Data
         [SerializeField] private Sprite sprite;
 
         public int SpawnDelay => spawnDelay;
+        public int MaxHealth => maxHealth;
 
         public int Damage => damage;
         public int AttackRange => attackRange;
