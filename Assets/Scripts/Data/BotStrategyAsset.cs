eb562475ed6ebe307e9b9eb4911cbe070edddcb3
Furkan Kambay <furkankambay@protonmail.com>
2026-07-10 20:40:58 +0300

    inline card list

--- Assets/Scripts/Data/BotStrategyAsset.cs
+++ Assets/Scripts/Data/BotStrategyAsset.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
 using FK.Deckowar.Deckbuilding;
 using UnityEngine;
+using Vertx.Attributes;
 
 namespace FK.Deckowar.Data
 {
     [CreateAssetMenu]
     public class BotStrategyAsset : ScriptableObject
     {
-        [SerializeField] private CardBundle[] cardBundles;
+        [SerializeField, Inline] private CardBundle[] cardBundles;
 
         public IReadOnlyList<CardBundle> CardBundles => cardBundles;
     }
