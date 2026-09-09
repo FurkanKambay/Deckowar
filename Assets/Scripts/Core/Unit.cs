88c0a2087ccfbf934811e023d38d1c03acb75393
Furkan Kambay <furkankambay@protonmail.com>
2026-09-09 23:05:28 +0300

    Serializable Unit

--- Assets/Scripts/Core/Unit.cs
+++ Assets/Scripts/Core/Unit.cs
@@ -1,15 +1,17 @@
 using System;
 using FK.Deckowar.Data;
+using UnityEngine;
 
 namespace FK.Deckowar.Core
 {
+    [Serializable]
     public sealed class Unit
     {
-        public UnitAsset UnitAsset { get; private set; }
-        public Intent Intent { get; private set; }
+        [field: SerializeField] public UnitAsset UnitAsset { get; private set; }
+        [field: SerializeField] public Faction Faction { get; private set; }
 
-        public Faction Faction { get; private set; }
         public Vitality Vitality { get; private set; }
+        public Intent Intent { get; private set; }
 
         public Unit(UnitAsset unitAsset, Faction faction)
         {
