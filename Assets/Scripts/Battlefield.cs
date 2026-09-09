1e2b6a4d5beda81199d22e8c4d2b39b7bc70ec45
Furkan Kambay <furkankambay@protonmail.com>
2026-09-09 23:02:55 +0300

    battlefield

--- Assets/Scripts/Battlefield.cs
+++ Assets/Scripts/Battlefield.cs
@@ -1,6 +1,8 @@
+using System;
 using FK.Common;
 using FK.Deckowar.Core;
 using UnityEngine;
+using Vertx.Attributes;
 
 namespace FK.Deckowar
 {
@@ -9,88 +11,86 @@ namespace FK.Deckowar
         [Header("Config")]
         [SerializeField, Range(2, 10)] private int cellCount = 5;
 
-        public int CellCount => cellCount;
+        [Header("Debug")]
+        [SerializeField, ReadOnlyField, Inline] private Unit[] units;
 
-        private Unit[,] units;
-        private int lastNavigableCell;
+        public int CellCount => cellCount;
 
         private void Awake()
         {
-            units = new Unit[2, cellCount - 1]; // 1 unnavigable cell on each side
-            //   2          3          4          5
-            // -----     -------   --------- -----------
-            // [0  ]     [0 1  ]   [0 1 2  ] [0 1 2 3  ]
-            // [  0]     [  1 0]   [  2 1 0] [  3 2 1 0]
-
-            lastNavigableCell = cellCount - 2;
+            units = new Unit[cellCount];
         }
 
-        public Unit GetUnit(Heading heading, int cell)
-        {
-            if (cell < 0 || cell > lastNavigableCell || heading is Heading.None)
-                return null;
-
-            return units[GetHeadingIndex(heading), cell];
-        }
-
-        public Unit GetOpponent(Heading heading, int cell) =>
-            GetUnit(heading.GetOpposite(), lastNavigableCell - cell);
-
-        public bool CanPushUnit(Heading heading) =>
-            heading is not Heading.None && GetUnit(heading, 0) == null;
-
-        public bool PushUnit(Heading heading, Unit unit)
-        {
-            if (!CanPushUnit(heading))
-                return false;
-
-            SetUnit(heading, 0, unit);
-            return true;
-        }
-
-        public bool RemoveUnit(Heading heading, int cell) =>
-            SetUnit(heading, cell, null);
-
+#region Advancing Units
         public void AdvanceAllUnits()
         {
+            Log.Info("Advancing all units", this);
             AdvanceUnits(Heading.East);
             AdvanceUnits(Heading.West);
         }
 
         private void AdvanceUnits(Heading heading)
         {
-            Log.Info($"Advancing {heading}ward units", this);
-
-            for (int cell = lastNavigableCell; cell >= 0; cell--)
+            for (int cell = units.Length; cell >= 0; cell--)
                 AdvanceUnit(heading, cell);
         }
 
-        public bool AdvanceUnit(Heading heading, int cell)
+        private bool AdvanceUnit(Heading heading, int cell)
         {
-            if (cell + 1 > lastNavigableCell)
+            if (cell < 0 || cell >= units.Length)
                 return false;
 
-            Unit unit = GetUnit(heading, cell);
-            Unit blockingUnit = GetUnit(heading, cell + 1);
-            Unit opponent = GetOpponent(heading, cell);
+            Unit unit = GetUnitAtCell(heading, cell);
+            if (unit is null) return false;
 
-            if (unit is null || blockingUnit is not null || opponent is not null)
+            Unit blockingUnit = GetUnitInFront(heading, cell);
+            if (blockingUnit is not null)
                 return false;
 
-            SetUnit(heading, cell + 1, unit);
-            return RemoveUnit(heading, cell);
+            bool moveSuccess = SetUnitAtCell(heading, cell + 1, unit);
+            if (!moveSuccess) return false;
+
+            bool success = SetUnitAtCell(heading, cell, null);
+            return success;
+        }
+#endregion
+
+        public bool CanPushUnit(Heading heading)
+        {
+            Unit unit = GetUnitAtCell(heading, 0);
+            return unit == null;
         }
 
-        private bool SetUnit(Heading heading, int cell, Unit unit)
+        public bool PushUnit(Heading heading, Unit unit)
         {
-            if (cell < 0 || cell >= cellCount || heading is Heading.None)
+            if (!CanPushUnit(heading))
+                return false;
+            return SetUnitAtCell(heading, 0, unit);
+        }
+
+        private Unit GetUnitInFront(Heading heading, int cell) =>
+            GetUnitAtCell(heading, cell + 1);
+
+        private Unit GetUnitAtCell(Heading heading, int cell)
+        {
+            if (heading is Heading.None || cell < 0 || cell >= units.Length)
+                return null;
+
+            Index index = heading is Heading.East ? cell : ^(cell + 1);
+            return units[index];
+        }
+
+        private bool RemoveUnitAtCell(Heading heading, int cell) =>
+            SetUnitAtCell(heading, cell, null);
+
+        private bool SetUnitAtCell(Heading heading, int cell, Unit unit)
+        {
+            if (heading is Heading.None || cell < 0 || cell >= units.Length)
                 return false;
 
-            units[GetHeadingIndex(heading), cell] = unit;
+            Index index = heading is Heading.East ? cell : ^(cell + 1);
+            units[index] = unit;
             return true;
         }
-
-        private static int GetHeadingIndex(Heading heading) =>
-            (int)heading - 1;
     }
 }
