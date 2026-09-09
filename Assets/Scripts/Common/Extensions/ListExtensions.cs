6aefb08df3aa02fadb9f4c4e93724422de8c65fb
Furkan Kambay <furkankambay@protonmail.com>
2026-09-09 12:08:27 +0300

    common

--- Assets/Scripts/Common/Extensions/ListExtensions.cs
+++ Assets/Scripts/Common/Extensions/ListExtensions.cs
@@ -1,29 +1,41 @@
+using System;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using JetBrains.Annotations;
+using Random = UnityEngine.Random;
 
 namespace FK.Common.Extensions
 {
     [PublicAPI]
     public static class ListExtensions
     {
-        private static RNGCryptoServiceProvider provider = new();
-        private static byte[] box = new byte[1];
-
-        public static void Shuffle<T>(this IList<T> list)
+        public static void ShuffleFast<T>(this IList<T> list)
         {
-            int n = list.Count;
-
-            while (n > 1)
+            for (int index = list.Count - 1; index > 0; index--)
             {
-                do
-                    provider.GetBytes(box);
-                while (!(box[0] < n * (byte.MaxValue / n)));
+                int newIndex = Random.Range(0, index + 1);
+                (list[newIndex], list[index]) = (list[index], list[newIndex]);
+            }
+        }
 
-                int k = box[0] % n;
-                n--;
+        public static void ShuffleSecure<T>(this IList<T> list)
+        {
+            int count = list.Count;
+            if (count > 255)
+                throw new ArgumentOutOfRangeException(nameof(list), $"Max 255 elements supported (got {count}).");
 
-                (list[k], list[n]) = (list[n], list[k]);
+            Span<byte> box = stackalloc byte[1];
+
+            for (int index = count - 1; index > 0; --index)
+            {
+                int range = index + 1; // 0..index
+                byte limit = (byte)(range * (byte.MaxValue / range)); // for unbiased modulo
+
+                do RandomNumberGenerator.Fill(box);
+                while (box[0] >= limit);
+
+                int newIndex = box[0] % range;
+                (list[newIndex], list[index]) = (list[index], list[newIndex]);
             }
         }
     }
--- Assets/Scripts/Common/Extensions/RemapExtensions.cs
+++ Assets/Scripts/Common/Extensions/RemapExtensions.cs
@@ -1,8 +1,21 @@
 using JetBrains.Annotations;
 using UnityEngine;
 
-namespace FK.Common
+namespace FK.Common.Extensions
 {
+    [PublicAPI]
+    public static class RemapExtensions
+    {
+        public static Remapping Remap(this float value, float min, float max) => new(value, min, max);
+        public static Remapping Remap(this int value, float min, float max) => new(value, min, max);
+
+        public static Remapping Remap01(this float value) => new(value, 0, 1);
+        public static Remapping Remap01(this int value) => new(value, 0, 1);
+
+        public static Remapping RemapPercent(this float value) => new(value, 0, 100);
+        public static Remapping RemapPercent(this int value) => new(value, 0, 100);
+    }
+
     [PublicAPI]
     public readonly struct Remapping
     {
